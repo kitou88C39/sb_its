abbab01aeb827b0476fbb4338b30c6710ef833a6
kitou88C39 <kanfutrooper@icloud.com>
2023-06-10 12:05:03 +0900

    issue/IssueSController.javaを編集

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,19 +1,21 @@
 package com.example.its.web.issue;
 
 import com.example.its.web.issue.IssueService;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     @GetMapping("/issues")
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
     }
-
 }
