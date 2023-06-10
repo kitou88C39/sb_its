5b7e7972284ea2198ab84e1be95a7ed2f86db15e
kitou88C39 <kanfutrooper@icloud.com>
2023-06-10 11:44:20 +0900

    IssueService.javaを編集

--- src/main/java/com/example/its/web/issue/IssueService.java
+++ src/main/java/com/example/its/web/issue/IssueService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class IssueService {
 
     public List<IssueEntity> findAll() {
-        return list of(
+        return List.of(
                 new IssueEntity(1, "概要1", "説明1"),
                 new IssueEntity(2, "概要2", "説明2"),
                 new IssueEntity(3, "概要3", "説明3"));
