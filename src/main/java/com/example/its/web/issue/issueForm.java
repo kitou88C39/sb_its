6daaec3f214e7ed840eab4990644181336a40ad1
kitou88C39 <kanfutrooper@icloud.com>
2023-06-14 10:19:18 +0900

    issues/IssueForm.javaを編集

--- src/main/java/com/example/its/web/issue/issueForm.java
+++ src/main/java/com/example/its/web/issue/issueForm.java
@@ -1,9 +1,17 @@
 package com.example.its.web.issue;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class issueForm {
+
+    @NotBlankBlank
+    @Size(max = 256)
     private String summary;
+
+    @NotBlankBlank
+    @Size(max = 256)
     private String description;
 }
